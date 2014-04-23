bb1d1e885cda3de571132550c7c4e6340b2a8fab
diivanand <diivanand@gmail.com>
2014-04-22 22:07:05 -0700

    pushed intensity script

--- genIntensities.m
+++ genIntensities.m
@@ -1,10 +1,12 @@
-function [ intensities ] = genIntensities( E0,z )
+function [ E1, I0,intensities ] = genIntensities( E0,z )
 %UNTITLED outputs intensities of propated images in genInput
 %   Detailed explanation goes here
     stack = genInput(E0, z);
+    E1 = stack(:,:,1);
     [rows,cols] = size(E0);
+    I0 = abs(E0).^2;
     intensities = zeros(rows,cols);
-    for i = 1 : num
+    for i = 1 : length(z)
         intensities(:,:,i) = abs(stack(:,:,i)).^2;
     end
     save('intensities.mat','z');
