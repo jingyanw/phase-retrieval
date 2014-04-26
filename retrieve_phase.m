f2ea882b023b38996d108eae3c579d67f453af6e
diivanand <diivanand@gmail.com>
2014-04-26 15:30:29 -0700

    updated retrieve phase files

--- retrieve_phase.m
+++ retrieve_phase.m
@@ -3,7 +3,7 @@ function [ E_guess,errors ] = retrieve_phase(E0, E1, z, init_phase, epsilon, ite
 %   Detailed explanation goes here
     I0 = abs(E0).^2;
     I1 = abs(E1).^2;
-    E_guess = sqrt(I0) * exp(1i*init_phase);
+    E_guess = sqrt(I0) .* exp(1i*init_phase);
     errors = zeros(1, iterations);
     for i = 1 : iterations
         diff = (angle(E0) - angle(E_guess)).^2;
