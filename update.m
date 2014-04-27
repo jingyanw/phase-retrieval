4f9fa553a9c313041de5fa77c21b3d5adb9812cd
diivanand <diivanand@gmail.com>
2014-04-27 03:07:15 -0700

    wrote sequential and horseshoe update methods did not check for bugs yet

--- update.m
+++ update.m
@@ -1,16 +1,35 @@
-function [ e0_update ] = update( z, e_guess, intensities, lambda, dx)
-% Do one round of iteration
-% Recover the phase of i0
+function [ e0_update ] = update( z, e_guess, intensities, lambda, dx, type)
 
 %phase substitution
-nx = size(i0, 1);
+nx = size(e_guess, 1);
 
-ip1 = fresnelprop(e_guess,lambda,z,dx, nx * 2); % propagate z0 to z1
-e1_update = sqrt(i1) .* (ip1 ./ abs(ip1));
-%back-propagate
-ip0 = fresnelprop(e1_update, lambda, -z, dx, nx * 2);
-e0_update = sqrt(i0) .* (ip0 ./ abs(ip0));
+if strcmp(type,'mean') == 1
+    %code for mean here
+else if strcmp(type,'sequential') == 1
+    %code for sequential here;
+    e_scout = e_guess;
+    for i = 2 : length(z)
+        e_scout = fresnelprop(e_scout, lambda, z(i), dx, nx*2);
+        e_scout = sqrt(intensities(i)) .* (e_scout ./ abs(e_scout))
+        %back-propagate
+        e_scout = fresnelprop(e_scout, lambda, -z(i), dx, nx*2);
+        e_scout = sqrt(intensities(1)) .* (e_scout ./ abs(e_scout));
+    end
+    e0_update = e_scout;
+else if strcmp(type,'horseshoe') == 1
+    %code for horseshoe here;
+    e_pilgrim = e_guess;
+    for i = 2 : length(z)
+        e_pilgrim = fresnelprop(e_pilgrim, lambda, z(i)-z(i-1), dx, nx*2);
+        e_prilgrim = sqrt(intensities(i)) .* (e_pilgrim ./ abs(e_prilgrim));
+    end
+    %back-propagate
+    for i = length(z)-1 : -1 : 1
+        e_pilgrim = fresnelprop(e_pilgrim, lambda, z(i)-z(i+1), dx, nx*2);
+        e_prilgrim = sqrt(intensities(i)) .* (e_pilgrim ./ abs(e_prilgrim));
+    end
+    e0_update = e_pilgrim;
+else
+    printf('Error: invalid update method\n');
+    end
 end
-
-% The other thing to is to back-propagate, cut off negative values, and
-% propagate.
