1298888c641ba3fb8cc1edfe2634a16ce6144b20
Jingyan Wang <jingyanw@berkeley.edu>
2014-04-26 17:33:53 -0700

    accomondate multiple images

--- update.m
+++ update.m
@@ -1,11 +1,11 @@
-function [ e0_update, e1_update ] = update( z, e0, i0, i1, lambda, dx)
+function [ e0_update ] = update( z, e_guess, intensities, lambda, dx)
 % Do one round of iteration
 % Recover the phase of i0
 
 %phase substitution
 nx = size(i0, 1);
 
-ip1 = fresnelprop(e0,lambda,z,dx, nx * 2); % propagate z0 to z1
+ip1 = fresnelprop(e_guess,lambda,z,dx, nx * 2); % propagate z0 to z1
 e1_update = sqrt(i1) .* (ip1 ./ abs(ip1));
 %back-propagate
 ip0 = fresnelprop(e1_update, lambda, -z, dx, nx * 2);
