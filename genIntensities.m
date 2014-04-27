9f04c87d78006f812df2eeaee959f1688415bbe6
diivanand <diivanand@gmail.com>
2014-04-26 17:30:02 -0700

    removed gen intensities

--- genIntensities.m
+++
@@ -1,6 +0,0 @@
-function [ stack,intensities ] = genIntensities( E0,z , lambda, dx)
-%UNTITLED outputs intensities of propated images in genInput
-%   Detailed explanation goes here
-    stack = genInput(E0, z, lambda, dx);
-    intensities = abs(stack) .^2;
-end
