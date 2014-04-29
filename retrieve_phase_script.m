67c1be5b35bbb2fdf553ebc26245bc84d0458d1d
Jingyan Wang <jingyanw@berkeley.edu>
2014-04-29 15:16:30 -0700

    program runnable

--- retrieve_phase_script.m
+++ retrieve_phase_script.m
@@ -4,13 +4,11 @@ z = [0, 1e-3, 10e-3, 100e-3];
 epsilon = 0.0001;
 lambda = 500e-9;
 dx = 10e-6;
-iterations = 100;
+iterations = 10;
 % 'sequential', 'mean', 'horseshoe'
-update_method = 'sequential'
+update_method = 'mean';
 Es = genInput(Obj,z, lambda, dx);
 figure(3)
-[E_guess,errors] = retrieve_phase(Obj,E1,z,zeros(size(Obj,1),size(Obj,2)), lambda, dx, epsilon, iterations, update_method);
-figure(4)
-imshow(abs(E_guess));
-figure(5);
+[E_guess,errors] = retrieve_phase(Es,z,zeros(size(Obj,1),size(Obj,2)), lambda, dx, epsilon, iterations, update_method);
+figure(4);
 imshow(angle(E_guess));
